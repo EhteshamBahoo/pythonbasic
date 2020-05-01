d758e5cc79bb505eccae0cc2439690b3472f0b90
EhteshamBahoo <ehteshambahoo9023@gmail.com>
2020-05-01 14:37:11 +0500

    Initial commit

---
+++ 1st.py
@@ -0,0 +1,52 @@
+print("Hello World")
+"""hash('hello')
+x = 23
+y = 32
+print(x+y)
+print(type(x))
+"""
+#values
+#int
+a = 9
+print(a)
+b = 98765434567898765434567876567 #dont start with zero
+print(b)
+#string
+c = 'A'
+print(c)
+d = 'billionier'
+print(d)
+#float
+e = 3.142
+print(e)
+#boolean
+f = True
+print(f)
+#null value
+g = None
+print(g)
+
+print(type(a))
+
+#experiments
+import keyword
+import builtins
+print(builtins)
+print(keyword.kwlist)
+
+_x = 1 #valid
+
+#multiple data insert
+a, b, c = 1, 2, 3
+print(a, b, c)
+A, B, C = 'hi','bye','wow'
+print(A, B, C)
+#array
+x = y = [20, 30, 40]
+print(x[0])
+
+#work
+def my_function():
+    a = 2
+    return a
+print(my_function())
